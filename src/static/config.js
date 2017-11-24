68321b45b499029b91f93dd1e7dfa976c2fe16bd
maartenplieger <maartenplieger@gmail.com>
2017-11-24 13:29:15 +0100

    Changed compute to portal

--- src/static/config.js
+++ src/static/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  'backendHost': 'https://compute-test.c3s-magic.eu:9001',
-  'adagucServicesHost': 'https://compute-test.c3s-magic.eu:8888'
+  'backendHost': 'https://portal.c3s-magic.eu:9001',
+  'adagucServicesHost': 'https://portal.c3s-magic.eu:8888'
 };
 
 try {
