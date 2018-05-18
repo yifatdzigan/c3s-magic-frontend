d2a9559e4c7bdea6f1699bfe74656246d0ecb90f
Faruk D <f.diblen@esciencecenter.nl>
2018-05-18 14:35:44 +0200

    added - anomaly agreement - partners section
    improved the layout

--- src/containers/Diagnostics/DiagnosticPage.jsx
+++ src/containers/Diagnostics/DiagnosticPage.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import MarkdownFromFile from '../MarkdownFromFile';
 
-//import YamlFromFile from '../YamlFromFile';
 import { YoutubeVideo, DiagnosticPlot } from './DiagnosticMedia';
 
 import ADAGUCViewerComponent from '../../components/ADAGUCViewerComponent';
@@ -17,59 +16,42 @@ var $RefParser = require('json-schema-ref-parser');
 
 export default class DiagnosticPage extends Component {
 
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      yamlData:'',
+      yamlData: '',
       readSuccess: false,
       yamlPath: ''
     };
     this.readYaml = this.readYaml.bind(this);
-//    this.renderPageElement = this.renderPageElement.bind(this);
- //   this.getKey = this.getKey.bind(this);
   }
 
 
 
-  readYaml () {
+  readYaml() {
     console.log("readYaml");
     console.log(this.props.yamlFile);
     var yamlPath = 'diagnosticsdata/' + this.props.yamlFile;
     console.log(yamlPath);
-    this.setState({yamlPath: yamlPath});
+    this.setState({ yamlPath: yamlPath });
     console.log(this.state);
 
     var that = this;
     $RefParser.dereference(yamlPath)
-    .then(data => {
-      console.log("read data: ", data);
-//      this.setState({yamlData: data});
-
-        that.setState({yamlData: data, readSuccess: true});
-
-
-      // if (this.props.configKey) {
-      //   var elementName = this.props.configKey;
-      //   var _result = this.renderPageElement(elementName);
-      //   this.setState({result: _result});
-      //   console.log(this.state);
-      // }
-      // else {
-      //   this.setState({result: 'key was not defined!'});
-      // }
-        console.log(this.state);
-    });
+      .then(data => {
+        that.setState({ yamlData: data, readSuccess: true });
+      });
 
   }
 
 
-  componentWillMount () {
+  componentWillMount() {
     console.log("componentWillMount");
     this.readYaml();
     console.log(this.state);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log("componentDidMount");
     console.log(this.state);
   }
@@ -77,11 +59,13 @@ export default class DiagnosticPage extends Component {
 
   renderPageElement(elementName) {
     if (this.state.readSuccess) {
-//      console.log(this.state);
+      //      console.log(this.state);
 
       var _element = '';
-
-      if (elementName === "authors") {
+      if (elementName === "partner") {
+        _element = this.state.yamlData[elementName];
+      }
+      else if (elementName === "authors") {
         _element = '<ul class="list-unstyled">';
         this.state.yamlData[elementName].forEach(function (element) {
           _element += '<li>' + element + '</li>';
@@ -139,122 +123,118 @@ export default class DiagnosticPage extends Component {
         _element = "No key was requested! Check the diagnostics settings.";
       }
 
-//      return _element;
-     return ( <div dangerouslySetInnerHTML={{ __html: _element }} />);
-
-      // console.log(_element);
-      // console.log(Parser(_element));
-      // return (Parser(_element));
-
+      return (<div dangerouslySetInnerHTML={{ __html: _element }} />);
     }
   }
 
 
 
-  render () {
-    console.log("render()");
-
-
-    console.log(this.state);
-    console.log(this.renderPageElement('map_data'));
-
-//    const mapData = config.backendHost + 'wms?DATASET=' + this.renderPageElement('map_data') + '&';
+  render() {
     const mapData = config.backendHost + 'wms?DATASET=' + 'anomaly_agreement_stippling' + '&';
-//    var mapData = 'https://portal.c3s-magic.eu/wms?DATASET=anomaly_agreement_stippling&';
-    console.log(mapData);
 
     if (this.state.readSuccess) {
-    return (
-      <div className='MainViewport'>
+      return (
+        <div className='MainViewport'>
 
-        <div className='text'>
-          <h2>
-            {this.renderPageElement('title')}
-          </h2>
-        </div>
+          <div className='text vspace2em'>
+            <h2>
+              {this.renderPageElement('title')}
+            </h2>
+          </div>
 
-        <Row>
-          <Col xs="6">
+          <Row>
+            <Col xs="6">
 
-            <div className='text'>
-              <h2>Description</h2>
-              {this.renderPageElement('description_short')}
-            </div>
+              <div className='text vspace2em'>
+                <h2>Patners</h2>
+                {this.renderPageElement('partner')}
+              </div>
 
-            <div>
-              <Button color="primary">Read more</Button>{' '}
-            </div>
+              <div className='text vspace2em'>
+                <h2>Description</h2>
+                {this.renderPageElement('description_short')}
+              </div>
 
-            <div className='text'>
-              <h2>Authors</h2>
-              {this.renderPageElement('authors')}
-            </div>
+              <div className='vspace2em'>
+                <Button color="primary">Read more</Button>{' '}
+              </div>
 
-            <div className='text'>
-              <h2>Reference</h2>
-              {this.renderPageElement('reference')}
-            </div>
+              <div className='text vspace2em'>
+                <h2>Authors</h2>
+                {this.renderPageElement('authors')}
+              </div>
 
-            <div>
-              <Button color="primary">Download report <Icon name='file-pdf' /></Button>{' '}
-              <Button color="primary">Download data <Icon name='file-archive' /></Button>{' '}
-            </div>
+              <div className='text vspace2em'>
+                <h2>Reference</h2>
+                {this.renderPageElement('reference')}
+              </div>
 
-            <div className='text'>
-              <h2>Settings</h2>
-              {this.renderPageElement('settings')}
-            </div>
+              <div className='vspace2em'>
+                <Button color="primary">Download report <Icon name='file-pdf' /></Button>{' '}
+                <Button color="primary">Download data <Icon name='file-archive' /></Button>{' '}
+              </div>
 
-            <div className='text'>
+              <div className='text vspace2em'>
+                <h2>Settings</h2>
+                {this.renderPageElement('settings')}
+              </div>
+
+              <div className='text vspace2em'>
                 <h2>Contact</h2>
                 {this.renderPageElement('contact')}
-            </div>
+              </div>
 
-          </Col>
+            </Col>
 
-          <Col xs="6">
+            <Col xs="6">
 
-            <div className="videoWrapper">
-              <YoutubeVideo video={this.renderPageElement('youtube')} autoplay="0" rel="0" modest="1" />
-            </div>
+              <div className="videoWrapper vspace2em">
+                <YoutubeVideo video={this.renderPageElement('youtube')} autoplay="0" rel="0" modest="1" />
+              </div>
 
-            <div className='text'>
+              <div className='text vspace2em'>
                 {mapData}
-            </div>
+              </div>
 
-            <div >
-              <ADAGUCViewerComponent
-                height={'50vh'}
-                stacklayers={true}
-                wmsurl={mapData}
-                parsedLayerCallback={ (wmjsregistry) => {
-                  this.wmjsregistry = wmjsregistry;
-                  if (!this.initialized) {
-                    this.initialized = true;
-                    this.wmjsregistry.anomaly.getLayers()[0].zoomToLayer(); } }
+              <div className='vspace2em'>
+                <ADAGUCViewerComponent
+                  height={'50vh'}
+                  stacklayers={true}
+                  wmsurl={mapData}
+                  parsedLayerCallback={(wmjsregistry) => {
+                    this.wmjsregistry = wmjsregistry;
+                    if (!this.initialized) {
+                      this.initialized = true;
+                      this.wmjsregistry.anomaly.getLayers()[0].zoomToLayer();
+                    }
                   }
-              />
-            </div>
+                  }
+                />
+              </div>
 
-            <div>
-              <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=512&h=360" alt="Card image cap" />
-              {this.renderPageElement('description_short')}
-            </div>
+              <div className='vspace2em'>
+                <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=512&h=360" alt="Card image cap" />
+              </div>
 
-            <div>
+              <div className='vspace2em'>
+                {this.renderPageElement('description_long')}
+              </div>
 
-            </div>
 
-          </Col>
+              <div>
 
-        </Row>
-      </div>);
+              </div>
+
+            </Col>
+
+          </Row>
+        </div>);
     }
     else {
-      return(
-      <div>
-        <p> This diagnostic pacge is not ready yet!</p>
-      </div>
+      return (
+        <div>
+          <p> This diagnostic pacge is not ready yet!</p>
+        </div>
       );
     }
 
