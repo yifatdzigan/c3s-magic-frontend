c5a37b0af73077fe60cd3bf5da53d3ecd698a3e3
Faruk D <f.diblen@esciencecenter.nl>
2018-05-25 16:08:13 +0200

    fix backendhost

--- src/static/config.js
+++ src/static/config.js
@@ -1,5 +1,5 @@
 const config = {};
-config.backendHost = 'https://portal.c3s-magic.eu'; // <-- If you dont run your own backend use the magic one
+config.backendHost = 'https://portal.c3s-magic.eu/'; // <-- If you dont run your own backend use the magic one
 // config.backendHost = 'https://localportal.c3s-magic.eu:7777';
 // config.backendHost = 'https://compute-test.c3s-magic.eu:9001/';
 
