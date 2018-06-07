5e048fb54b0fdcd6b19ad8ee62d78e20e5739556
maartenplieger <maartenplieger@gmail.com>
2018-06-07 17:30:48 +0200

    Fixed url names

--- src/containers/AdagucViewerContainer.jsx
+++ src/containers/AdagucViewerContainer.jsx
@@ -10,63 +10,63 @@ export default class AdagucViewerContainer extends Component {
     this.state = {
       datasets: [
       {
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP4-6_ISAC_hyint&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP4-6_ISAC_hyint&',
         name:'WP4-6_ISAC_hyint',
         title:'WP4-6_ISAC_hyint'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP4-6_ISAC_miles&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP4-6_ISAC_miles&',
         name:'WP4-6_ISAC_miles',
         title:'WP4-6_ISAC_miles'
       },/*{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP4-6_ISAC_zmnam&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP4-6_ISAC_zmnam&',
         name:'WP4-6_ISAC_zmnam',
         title:'WP4-6_ISAC_zmnam'
       },*/{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP4_BSC_Modes of variability&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP4_BSC_Modes of variability&',
         name:'WP4_BSC_Modes_of_variability',
         title:'WP4_BSC_Modes_of_variability'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP4_ISAC_quantilebias&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP4_ISAC_quantilebias&',
         name:'WP4_ISAC_quantilebias',
         title:'WP4_ISAC_quantilebias'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP5_ISAC_ensclus&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP5_ISAC_ensclus&',
         name:'WP5_ISAC_ensclus',
         title:'WP5_ISAC_ensclus'
       },/*{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP6_BSC_COMBINATION_AND_AREA_AVERAGING&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP6_BSC_COMBINATION_AND_AREA_AVERAGING&',
         name:'WP6_BSC_COMBINATION_AND_AREA_AVERAGING',
         title:'WP6_BSC_COMBINATION_AND_AREA_AVERAGING'
       },*/{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP6_ISAC_teleconnection&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP6_ISAC_teleconnection&',
         name:'WP6_ISAC_teleconnection',
         title:'WP6_ISAC_teleconnection'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/adagucserver?source=c3smagic%2FWP6_KNMI_Correlations%2Fclimexp_correlate_output.nc&',
+        wmsurl:'https://portal.c3s-magic.eu/adagucserver?source=c3smagic%2FWP6_KNMI_Correlations%2Fclimexp_correlate_output.nc&',
         name:'WP6_KNMI_correlations',
         title:'WP6_KNMI_correlations'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP7_BSC_ACI_Components&',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP7_BSC_ACI_Components&',
         name:'WP7_BSC_ACI_Components',
         title:'WP7_BSC_ACI_Components'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP7_BSC_Energy',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP7_BSC_Energy',
         name:'WP7_BSC_Energy',
         title:'WP7_BSC_Energy'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP7-drought_indicator',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP7-drought_indicator',
         name:'WP7-drought_indicator',
         title:'WP7-drought_indicator'
       },/*{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP7_shapefileselector',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP7_shapefileselector',
         name:'WP7_shapefileselector',
         title:'WP7_shapefileselector'
       },*/{
-        wmsurl:'https://localportal.c3s-magic.eu:7777/wms?DATASET=WP7_ISAC_rainfarm',
+        wmsurl:'https://portal.c3s-magic.eu/wms?DATASET=WP7_ISAC_rainfarm',
         name:'WP7_ISAC_rainfarm',
         title:'WP7_ISAC_rainfarm'
       },{
-        wmsurl:'https://localportal.c3s-magic.eu:7777:443/adagucserver?source=c3smagic%2FWP7-surge_estimator%2Fsurge_heights_EC-Earth_s01r14_20131201-20140131.nc&',
+        wmsurl:'https://portal.c3s-magic.eu:443/adagucserver?source=c3smagic%2FWP7-surge_estimator%2Fsurge_heights_EC-Earth_s01r14_20131201-20140131.nc&',
         name:'WP7_surge_estimator',
         title:'WP7_surge_estimator'
       }
--- src/static/config.js
+++ src/static/config.js
@@ -1,7 +1,7 @@
 const config = {};
-// config.backendHost = 'https://portal.c3s-magic.eu/'; // <-- If you dont run your own backend use the magic one
+config.backendHost = 'https://portal.c3s-magic.eu/'; // <-- If you dont run your own backend use the magic one
 config.adagucViewerURL = 'http://portal.c3s-magic.eu:8080/adaguc-viewer/';
-config.backendHost = 'https://localportal.c3s-magic.eu:7777';
+// config.backendHost = 'https://localportal.c3s-magic.eu:7777';
 // config.backendHost = 'https://compute-test.c3s-magic.eu:9001/';
 
 // export const config = { 'backendHost': 'https://compute-test.c3s-magic.eu:7777', 'adagucServicesHost': 'https://compute-test.c3s-magic.eu:8888' };
