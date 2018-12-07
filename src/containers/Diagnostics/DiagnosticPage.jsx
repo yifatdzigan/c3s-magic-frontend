693a3badfece94455e0ff4cb15c4532011c44353
Faruk D <f.diblen@esciencecenter.nl>
2018-12-07 14:36:09 +0100

    remove duplicate of settings section

--- src/containers/Diagnostics/DiagnosticPage.jsx
+++ src/containers/Diagnostics/DiagnosticPage.jsx
@@ -254,12 +254,6 @@ class DiagnosticPage extends Component {
                   : null
                 }
               </div>
-
-              <div className='text vspace2em'>
-                <h2 style={{ color: '#921A36'}}>Settings</h2>
-                {this.renderPageElement('settings')}
-                <Button color="primary" className="disabled"><Icon name='' />&nbsp;Change Settings</Button>{' '}
-              </div>
             </Col>
           </Row>
 
