c053beacff3402b445021e821ec872e73a353332
Maarten Plieger <maartenplieger@gmail.com>
2018-12-13 15:33:02 +0100

    Added descriptions to tailored products

--- src/containers/TailoredProducts/TPCatchmentSelector.jsx
+++ src/containers/TailoredProducts/TPCatchmentSelector.jsx
@@ -257,7 +257,8 @@ class TPCatchmentSelector extends Component {
       <h1>Hydrology - catchment selector</h1>
       <Row>
         <div className='text' style={{ paddingBottom:'15px' }}>
-        BLA BLA BLA ... <span>{this.state.featureIndex}</span>
+          Impact modelers are often interested in data for irregular regions best defined by a shapefile. With the shapefile selector tool, the user can extract time series or CII data for a user defined region. The region is defined by a user provided shapefile that includes one or several polygons. For each polygon, a new timeseries, or CII, is produced with only one time series per polygon. The spatial information is reduced to a representative point for the polygon ('representative') or as an average of all grid points within the polygon boundaries ('mean_inside'). If there are no grid points strictly inside the polygon, the 'mean_inside' method defaults to 'representative' for that polygon. An option for displaying the grid points together with the shapefile polygon allows the user to assess which method is most optimal. In case interpolation to a high input grid is necessary, this can be provided in a pre-processing stage. Outputs are in the form of a NetCDF file, or as ascii code in csv format.
+          &nbsp;Please check the <a href='/#/diagnostics/shapefile_selection'>shapefile selection metric</a> to calculate.
         </div>
       </Row>
       <Row>
